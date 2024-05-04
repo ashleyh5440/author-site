010746b10649f89486b41f2df8d2b5a9343a590d
Ashley <ashleyh5440@gmail.com>
2024-05-03 20:40:41 -0400

    added initial files

---
+++ src/main.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App.jsx'
+import './index.css'
+
+import App from './App.jsx';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import About from './pages/About';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+      {
+        path: '/contact',
+        element: <Contact />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
