c925bb947de126428685d1bbb593e03ce99f1b2f
Ashley <ashleyh5440@gmail.com>
2024-05-06 19:55:24 -0400

    fixed some spacing

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,7 +3,7 @@ import footerLogo from '../../../public/images/footer-logo.png';
 function Footer() {
     return(
         <section>
-            <div style={{backgroundColor: "rgb(42, 4, 52)", height: "50px", textAlign: "center", padding: "1%", display: "flex", flexDirection: "row", justifyContent: "center"}}>
+            <div style={{backgroundColor: "rgb(42, 4, 52)", height: "50px", textAlign: "center", padding: "1%", display: "flex", flexDirection: "row", flexWrap: "wrap", justifyContent: "center"}}>
                 <p>Designed and developed by <a href="https://ashleycreates.org/" target="_blank">ashleycreates.org</a></p>
                 <img src={footerLogo} style={{width: "35px", height:"15px", marginLeft: "10px"}}/>
             </div>
